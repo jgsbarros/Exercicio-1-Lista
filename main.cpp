c368505ac175513f358869edbe3c3d4267a837b1
jgsbarros <41957952+jgsbarros@users.noreply.github.com>
2018-09-08 20:18:33 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int a = 3;
+    int b = 4;
+    int r = a + b;
+    printf("%i\n", a);
+    printf("%i\n", b);
+    printf("a + b = %d + %d = %d\n", a, b, r);
+
+    return 0;
+}
